afbfeb3f4b86d4779d8a695bd6b7203e53d839e2
Joshua Hawcroft <dev@joshhawcroft.com>
2013-10-27 12:31:20 +1100

    initial commit

---
+++ src/tests/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  BRAIN
+//
+//  Created by Joshua Hawcroft on 27/10/2013.
+//  Copyright (c) 2013 Joshua Hawcroft. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
